d542ff4b8c1b13c70f12460a7a6f07c4561ff752
MKA1500 <maciejkabsch@gmail.com>
2016-08-04 21:52:34 +0200

    ...

--- functions.php
+++ functions.php
@@ -6,4 +6,11 @@ function mktheme_resources() {
 
 add_action('wp_enqueue_scripts', 'mktheme_resources');
 
+// Navigation Menus
+
+register_nav_menus(array(
+    'primary' => __('Primary Menu'),
+	'footer' => __('Footer Menu')
+));
+
  ?>
